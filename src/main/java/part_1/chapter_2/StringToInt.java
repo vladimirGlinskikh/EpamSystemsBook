faaab965e9ce4f5c89255ac6d56f7c4d215455ba
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-28 05:25:21 +0600

    обратное преобразование из числа в строку

--- src/main/java/part_1/chapter_2/StringToInt.java
+++ src/main/java/part_1/chapter_2/StringToInt.java
@@ -3,13 +3,22 @@ package part_1.chapter_2;
 public class StringToInt {
     public static void main(String[] args) {
         String arg = "71";
+        int value = 71;
         try {
             int value1 = Integer.parseInt(arg);// возвращает int
             int value2 = Integer.valueOf(arg); // возвращает Integer
             int value3 = Integer.decode(arg); // возвращает Integer
+            // обратное преобразование из int в String
+            String arg1 = Integer.toString(value);
+            String arg2 = String.valueOf(value);
+            String arg3 = "" + value; // плохой способ
             System.out.println(value1);
             System.out.println(value2);
             System.out.println(value3);
+            System.out.println("--------------------");
+            System.out.println(arg1);
+            System.out.println(arg2);
+            System.out.println(arg3);
         } catch (NumberFormatException e) {
             System.err.println("Неверный формат числа " + e);
         }
