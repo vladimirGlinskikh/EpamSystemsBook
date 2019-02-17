fa09c5496ef62808e9bf4f44b96f507f1dcfc528
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-18 04:34:47 +0600

    Обращение к статическому методу и константе.

---
+++ src/main/java/part_1/chapter_4/stat/ImportDemo.java
@@ -0,0 +1,8 @@
+package part_1.chapter_4.stat;
+
+public class ImportDemo {
+    public static void main(String[] args) {
+        System.out.println(2 * Math.PI * 3);
+        System.out.println(Math.floor(Math.cos(Math.PI / 3)));
+    }
+}
