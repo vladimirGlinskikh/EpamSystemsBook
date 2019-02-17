80b0b2e9ee53099651bb3f1d8ccdba93af7bb40e
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-17 15:12:50 +0600

    Абстрактный класс и метод

---
+++ src/main/java/part_1/chapter_4/inheritance/AbstractCardAction.java
@@ -0,0 +1,14 @@
+package part_1.chapter_4.inheritance;
+
+public abstract class AbstractCardAction {
+    private int id;
+
+    public AbstractCardAction() {
+    }
+
+    public boolean checkLimit() {
+        return true;
+    }
+
+    public abstract void doPayment(double amoutPayment);
+}
