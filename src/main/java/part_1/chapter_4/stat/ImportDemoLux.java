5e8b4c09ad3094705ab5740cda8a5311f88267e0
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-18 04:38:44 +0600

    Статический импорт методов и констант

---
+++ src/main/java/part_1/chapter_4/stat/ImportDemoLux.java
@@ -0,0 +1,10 @@
+package part_1.chapter_4.stat;
+
+import static java.lang.Math.*;
+
+public class ImportDemoLux {
+    public static void main(String[] args) {
+        System.out.println(2 * PI * 3);
+        System.out.println(floor(cos(PI / 3)));
+    }
+}
