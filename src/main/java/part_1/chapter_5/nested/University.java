eb88e620ee91459ce6a43d93f94e73fb923ef608
VladimirGlinskikh <support@zhelezyaka.kz>
2019-02-19 23:04:25 +0600

    Класс вложенный в интерфейс

---
+++ src/main/java/part_1/chapter_5/nested/University.java
@@ -0,0 +1,19 @@
+package part_1.chapter_5.nested;
+
+public interface University {
+    int NUMBER_FACULTY = 20;
+
+    void create();
+
+    class LearningDepartment {
+        public int idChief;
+
+        public static void assignPlan(int idFaculty) {
+            // TODO
+        }
+
+        public void acceptProgram() {
+            // TODO
+        }
+    }
+}
