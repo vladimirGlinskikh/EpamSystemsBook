bacde4d98e1d352ef575c320d0bcf10b45943c84
VladimirGlinskikh <support@zhelezyaka.kz>
2019-02-19 23:33:29 +0600

    Анонимные классы

---
+++ src/main/java/part_1/chapter_5/anonym/WrapperString.java
@@ -0,0 +1,25 @@
+package part_1.chapter_5.anonym;
+
+public class WrapperString {
+    private String str;
+
+    public WrapperString(String str) {
+        this.str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+
+    public String replace(char oldChar, char newChar) {
+        char[] array = new char[str.length()];
+        str.getChars(0, str.length(), array, 0);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == oldChar) {
+                array[i] = newChar;
+                break;
+            }
+        }
+        return new String(array);
+    }
+}
