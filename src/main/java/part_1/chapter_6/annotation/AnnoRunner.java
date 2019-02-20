959ffa0aec53ceb9faa746cd66e585e40c04de82
VladimirGlinskikh <support@zhelezyaka.kz>
2019-02-20 15:22:11 +0600

    Конфигурирование и запуск

--- src/main/java/part_1/chapter_6/annotation/AnnoRunner.java
+++ src/main/java/part_1/chapter_6/annotation/AnnoRunner.java
@@ -5,6 +5,10 @@ import part_1.chapter_6.accountLogic.AccountOperationManager;
 public class AnnoRunner {
     public static void main(String[] args) {
         AccountOperationManager account = new AccountOperationManagerImpl();
-        AccountOperationManager securityAccount =
+        AccountOperationManager securityAccount = SecurityFactory.createSecurityObject(account);
+        securityAccount.depositInCash(10128336, 6);
+        securityAccount.withDraw(64092376, 2);
+        securityAccount.convert(200);
+        securityAccount.transfer(64092376, 300);
     }
 }
