bebdd79793209378a5ffded5eea0aeb6f590e083
VladimirGlinskikh <support@zhelezyaka.kz>
2019-02-20 15:05:22 +0600

    Логика обработки значения аннотации

---
+++ src/main/java/part_1/chapter_6/annotation/AnnoRunner.java
@@ -0,0 +1,10 @@
+package part_1.chapter_6.annotation;
+
+import part_1.chapter_6.accountLogic.AccountOperationManager;
+
+public class AnnoRunner {
+    public static void main(String[] args) {
+        AccountOperationManager account = new AccountOperationManagerImpl();
+        AccountOperationManager securityAccount =
+    }
+}
