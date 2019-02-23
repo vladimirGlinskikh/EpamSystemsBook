bb0e3b013b4430150667b3ee0907f7c22bbfd022
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-23 16:08:12 +0600

    Менеджер ресурсов

---
+++ src/main/java/part_2/chapter_7/resource/HamletInternational.java
@@ -0,0 +1,38 @@
+package part_2.chapter_7.resource;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class HamletInternational {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("1 - английский \n 2 - белорусский \n любой - русский ");
+            char j = 0;
+            try {
+                j = (char) System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String country = "";
+            String language = "";
+            switch (j) {
+                case '1':
+                    country = "US";
+                    language = "EN";
+                    break;
+                case '2':
+                    country = "BY";
+                    language = "BE";
+                    break;
+            }
+            Locale current = new Locale(language, country);
+            ResourceBundle rb = ResourceBundle.getBundle("property.text", current);
+            String s1 = rb.getString("str1");
+            System.out.println(s1);
+
+            String s2 = rb.getString("str2");
+            System.out.println(s2);
+        }
+    }
+}
