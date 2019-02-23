a79acbe0635f9c0bb5641de042df4233b33a6ae1
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-24 04:50:41 +0600

    Форматирование даты и времени

---
+++ src/main/java/part_2/chapter_7/format/FormatterDemoTimeAndDate.java
@@ -0,0 +1,26 @@
+package part_2.chapter_7.format;
+
+import java.util.Calendar;
+import java.util.Formatter;
+
+public class FormatterDemoTimeAndDate {
+    public static void main(String[] args) {
+        Formatter f = new Formatter();
+        Calendar cal = Calendar.getInstance();
+
+        f.format("%tr", cal);
+        System.out.println(f);
+
+        f = new Formatter();
+        f.format("%tc", cal);
+        System.out.println(f);
+
+        f = new Formatter();
+        f.format("%tl:%tM", cal, cal);
+        System.out.println(f);
+
+        f = new Formatter();
+        f.format("%tB %tb %tm", cal, cal, cal);
+        System.out.println(f);
+    }
+}
