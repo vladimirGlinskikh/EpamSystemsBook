0c1c8e4e8cd654c72ccd0372d489bea7062a15f3
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-02-28 10:57:53 +0600

    Сериализуемый класс.

---
+++ src/main/java/part_2/chapter_9/serial/Student.java
@@ -0,0 +1,22 @@
+package part_2.chapter_9.serial;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    protected static String faculty;
+    private String name;
+    private int id;
+    private transient String password;
+    private static final long serialVersionUID = 1L;
+
+    public Student(String nameOfFaculty, String name, int id, String password) {
+        faculty = nameOfFaculty;
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String toString() {
+        return "\nfaculty " + faculty + "\nname " + name + "\nID " + id + "\npassword " + password;
+    }
+}
