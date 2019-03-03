4c8e548901b61d2be27d6b857dcc0ea31a1e0eaf
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-04 02:06:42 +0600

    Загрузка файла properties в экземпляр и доступ к содержимому.

---
+++ src/main/java/part_2/chapter_10/collection/PropertiesDemo.java
@@ -0,0 +1,23 @@
+package part_2.chapter_10.collection;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesDemo {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        try {
+            props.load(new FileReader("prop/database.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String driver = props.getProperty("db.driver");
+        String maxIdle = props.getProperty("db.maxIdle");
+        String maxActive = props.getProperty("db.maxActive", "20");
+        System.out.println(driver);
+        System.out.println(maxIdle);
+        System.out.println(maxActive);
+    }
+}
