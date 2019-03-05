5e2e8206e3cb6dcebf18838bd0b54c4686744268
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-06 02:43:49 +0600

    Пул ресурсов.

---
+++ src/main/java/part_2/chapter_11/resource/pool/ChannelPool.java
@@ -0,0 +1,33 @@
+package part_2.chapter_11.resource.pool;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class ChannelPool<T> {
+    private final static int POOL_SIZE = 5;
+    private final Semaphore semaphore = new Semaphore(POOL_SIZE, true);
+    private final Queue<T> resources = new LinkedList<>();
+
+    public ChannelPool(Queue<T> source) {
+        resources.addAll(source);
+    }
+
+    public T getResource(long maxWaitMillis) throws ResourceException {
+        try {
+            if (semaphore.tryAcquire(maxWaitMillis, TimeUnit.MILLISECONDS)) {
+                T res = resources.poll();
+                return res;
+            }
+        } catch (InterruptedException e) {
+            throw new ResourceException(e);
+        }
+        throw new ResourceException(":превышено время ожидания");
+    }
+
+    public void returnResource(T res) {
+        resources.add(res);
+        semaphore.release();
+    }
+}
