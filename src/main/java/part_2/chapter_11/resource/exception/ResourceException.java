098552cb67ead62b87b88daa815efec93a1544ab
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-06 02:51:42 +0600

    Исключение, информирующее о сбое в поставке ресурса.

---
+++ src/main/java/part_2/chapter_11/resource/exception/ResourceException.java
@@ -0,0 +1,19 @@
+package part_2.chapter_11.resource.exception;
+
+public class ResourceException extends Exception {
+    public ResourceException() {
+        super();
+    }
+
+    public ResourceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceException(String message) {
+        super(message);
+    }
+
+    public ResourceException(Throwable cause) {
+        super(cause);
+    }
+}
