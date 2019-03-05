de1edbfe47765e8c9bdaf79723d0ade9ea683ab0
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-06 03:58:28 +0600

    Поток использующий барьер.

---
+++ src/main/java/part_2/chapter_11/auction/Bid.java
@@ -0,0 +1,42 @@
+package part_2.chapter_11.auction;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Bid extends Thread {
+    private Integer bidId;
+    private int price;
+    private CyclicBarrier barrier;
+
+    public Bid(int id, int price, CyclicBarrier barrier) {
+        this.bidId = id;
+        this.price = price;
+        this.barrier = barrier;
+    }
+
+    public Integer getBidId() {
+        return bidId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Client " + this.bidId + " specifies a price.");
+            Thread.sleep(new Random().nextInt(3000));
+            int delta = new Random().nextInt(50);
+            price += delta;
+            System.out.println("Bid " + this.bidId + " : " + price);
+            this.barrier.await();
+            System.out.println("Continue to work...");
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
