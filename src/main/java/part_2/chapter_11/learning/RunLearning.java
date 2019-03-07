eb8dc4db861a5fd7454c9f5a3d4ebb466893b77c
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-07 11:44:37 +0600

    Запуск формирования группы студентов и выполнения проверки их заданий.

---
+++ src/main/java/part_2/chapter_11/learning/RunLearning.java
@@ -0,0 +1,36 @@
+package part_2.chapter_11.learning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunLearning {
+    public static void main(String[] args) {
+        final int NUMBER_TASKS_1 = 5;
+        Student student1 = new Student(322801, NUMBER_TASKS_1);
+        for (int i = 0; i < NUMBER_TASKS_1; i++) {
+            Task t = new Task("Task #" + i);
+            student1.addTask(t);
+        }
+
+        final int NUMBER_TASKS_2 = 4;
+        Student student2 = new Student(322924, NUMBER_TASKS_2);
+        for (int i = 0; i < NUMBER_TASKS_2; i++) {
+            Task t = new Task("Task ##" + i);
+            student2.addTask(t);
+        }
+
+        List<Student> lst = new ArrayList<>();
+        lst.add(student1);
+        lst.add(student2);
+        Tutor tutor = new Tutor(lst);
+        student1.start();
+        student2.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        tutor.start();
+    }
+}
