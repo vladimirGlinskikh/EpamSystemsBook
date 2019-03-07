84ae7951b4b8ce8d69c57acb8a7ce48fb66f85d5
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-08 04:09:20 +0600

    Запуск пула потоков и извлечение результатов их работы.

---
+++ src/main/java/part_2/chapter_11/future/RunExecutor.java
@@ -0,0 +1,21 @@
+package part_2.chapter_11.future;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class RunExecutor {
+    public static void main(String[] args) throws Exception {
+        List<Future<String>> list = new ArrayList<>();
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 7; i++) {
+            list.add(es.submit(new BaseCallable()));
+        }
+        es.shutdown();
+        for (Future<String> future : list) {
+            System.out.println(future.get() + " result fixed");
+        }
+    }
+}
