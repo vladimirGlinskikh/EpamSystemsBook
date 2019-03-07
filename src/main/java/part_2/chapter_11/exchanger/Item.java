86ccb626b769caa3df54094e7175a8c4840c61ea
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-07 15:14:33 +0600

    Класс-описание товара.

---
+++ src/main/java/part_2/chapter_11/exchanger/Item.java
@@ -0,0 +1,20 @@
+package part_2.chapter_11.exchanger;
+
+public class Item {
+    private Integer id;
+    private Integer number;
+
+    public Item(Integer id, Integer number) {
+        super();
+        this.id = id;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+}
