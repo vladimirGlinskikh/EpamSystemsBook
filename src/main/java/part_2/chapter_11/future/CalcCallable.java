03ee1c6d60ae7a7b561a474b54e7d5b0d5d86a70
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-08 03:54:44 +0600

    Поток с возвращением результата.

---
+++ src/main/java/part_2/chapter_11/future/CalcCallable.java
@@ -0,0 +1,12 @@
+package part_2.chapter_11.future;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class CalcCallable implements Callable<Number> {
+    @Override
+    public Number call() throws Exception {
+        Number res = new Random().nextGaussian();
+        return res;
+    }
+}
