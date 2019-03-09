a9983437fcce4a8ddc409a014e7b4fd2b8a235c0
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-09 17:17:28 +0600

    Подготовка запроса на добавление информации.

---
+++ src/main/java/part_2/chapter_12/data/connect/ConnectorDB.java
@@ -0,0 +1,16 @@
+package part_2.chapter_12.data.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectorDB {
+    public static Connection getConnection() throws SQLException {
+        ResourceBundle resource = ResourceBundle.getBundle("database");
+        String url = resource.getString("db.url");
+        String user = resource.getString("db.user");
+        String pass = resource.getString("db.password");
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
