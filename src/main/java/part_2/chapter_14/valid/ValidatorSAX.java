4c5a3a875dca3376c4942000d6e1ffb39cd50353
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-11 12:28:23 +0600

    Проверка корректности документа.

---
+++ src/main/java/part_2/chapter_14/valid/ValidatorSAX.java
@@ -0,0 +1,37 @@
+package part_2.chapter_14.valid;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class ValidatorSAX {
+    public static void main(String[] args) {
+        String filename = "data/students.xml";
+        String schemaname = "data/students.xsd";
+        String logname = "logs/log.txt";
+        Schema schema = null;
+        String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory factory = SchemaFactory.newInstance(language);
+        try {
+            schema = factory.newSchema(new File(schemaname));
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setSchema(schema);
+            SAXParser parser = spf.newSAXParser();
+            parser.parse(filename, new StudentErrorHandler(logname));
+            System.out.println(filename + " is valid");
+        } catch (ParserConfigurationException e) {
+            System.err.println(filename + " config error: " + e.getMessage());
+        } catch (SAXException e) {
+            System.err.println(filename + " SAX error: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("I/O error: " + e.getMessage());
+        }
+    }
+}
