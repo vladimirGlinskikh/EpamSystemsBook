91b804b2d9bd8ce31a6c55fcc649dc0e268a4ffe
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-13 10:19:58 +0600

    Класс CurrentStateTest будет владеть информацией о текущем состоянии теста.

---
+++ src/main/java/part_4/expert/CurrentStateTest.java
@@ -0,0 +1,9 @@
+package part_4.expert;
+
+public class CurrentStateTest {
+    private int idT;
+    private int idS;
+    private int idCurrentQuest;
+    private long timeRemain;
+    private Quest<Long> idListQuest;
+}
--- src/main/java/part_4/expert/Quest.java
+++ src/main/java/part_4/expert/Quest.java
@@ -1,7 +1,7 @@
 package part_4.expert;
 
 // информационный эксперт
-public class Quest {
+public class Quest<L extends Number> {
     private int idQuest;
     private int idTest;
     // реализация конструкторов и методов
