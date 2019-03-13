8c24965e2ff4f143873e937d7aa4c7c8f58bca89
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-13 10:15:37 +0600

    Шаблон эксперт.

---
+++ src/main/java/part_4/expert/Quest.java
@@ -0,0 +1,8 @@
+package part_4.expert;
+
+// информационный эксперт
+public class Quest {
+    private int idQuest;
+    private int idTest;
+    // реализация конструкторов и методов
+}
---
+++ src/main/java/part_4/expert/Test.java
@@ -0,0 +1,10 @@
+package part_4.expert;
+
+public class Test {
+    // информационный эксперт
+    private int idTest;
+    private String testName;
+    private int questNumber;
+    private long time;
+    // реализация конструкторов и методов
+}
