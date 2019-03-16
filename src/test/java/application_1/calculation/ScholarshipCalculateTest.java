780d563cd410b9b264eb5e86f95cf725119028a4
Vladimir Glinskikh <support@zhelezyaka.kz>
2019-03-16 14:50:37 +0600

    Тест с аннотацией @Test

---
+++ src/test/java/application_1/calculation/ScholarshipCalculateTest.java
@@ -0,0 +1,19 @@
+package application_1.calculation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScholarshipCalculateTest {
+    @Test
+    public void scholarshipCalculateTest() {
+        IscholarshipCalculator scholarshipCalculator =
+                new ScholarshipCalculatorImpl();
+        double basicScholarship = ScholarshipCalculatorImpl.BASIC_SCHOLARSHIP;
+        double stepUpCoefficient = 1.1;
+        double expected = basicScholarship * stepUpCoefficient;
+        double actual = scholarshipCalculator.scholarshipCalculate(stepUpCoefficient);
+
+        Assert.assertEquals(expected, actual, 0.01);
+        
+    }
+}
