413b4e881704fdfc50e2f6dc55169e1089fb0572
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-17 18:46:25 +0600

    Запуск набора тестов.

---
+++ src/test/java/application_1/calculation/ScholarshipSuite.java
@@ -0,0 +1,10 @@
+package application_1.calculation;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({ScholarshipCalculateTest.class, ScholarshipCalculatorTestTime.class})
+@RunWith(Suite.class)
+public class ScholarshipSuite {
+}
