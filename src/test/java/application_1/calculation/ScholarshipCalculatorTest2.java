248dcc33871685a614a0da2671309ee7aaac35cc
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-17 15:07:12 +0600

    Метод тестирования сообщения в исключении после его генерации.

--- src/test/java/application_1/calculation/ScholarshipCalculatorTest2.java
+++ src/test/java/application_1/calculation/ScholarshipCalculatorTest2.java
@@ -1,5 +1,6 @@
 package application_1.calculation;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -14,16 +15,27 @@ public class ScholarshipCalculatorTest2 {
     }
 
     @Test(expected = NotSuchMarkException.class)
-    public void stepUpCoefficientForFiveTest() throws NotSuchMarkException{
+    public void stepUpCoefficientForFiveTest() throws NotSuchMarkException {
         double expected = 1.5;
         double actual = scholarshipCalculator.stepUpCoefficientCalculate(5);
         assertEquals("Coefficient for mark 5 is wrong: ", expected, actual, 0.01);
     }
 
     @Test(expected = NotSuchMarkException.class)
-    public void stepUpCoefficientThreeTest() throws  NotSuchMarkException{
+    public void stepUpCoefficientThreeTest() throws NotSuchMarkException {
         double expected = 1;
         double actual = scholarshipCalculator.stepUpCoefficientCalculate(3);
         assertEquals("Coefficient for mark 3 is wrong: ", expected, actual, 0.01);
+    }
+
+    @Test
+    public void stepUpCoefficientElevenTest() {
+        int averageMark = 11;
+        try {
+            scholarshipCalculator.stepUpCoefficientCalculate(averageMark);
+            fail("Test for mark " + averageMark + " should have thrown a NotSuchMarkException");
+        } catch (NotSuchMarkException e) {
+            Assert.assertEquals("There is no mark: " + averageMark, e.getMessage());
+        }
     }
 }
