1f978172f7d1c25bbe9b1a1e58f425723bdc4767
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-17 18:01:19 +0600

    Тест с ограничением по времени.

---
+++ src/test/java/application_1/calculation/ScholarshipCalculatorTestTime.java
@@ -0,0 +1,24 @@
+package application_1.calculation;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ScholarshipCalculatorTestTime {
+    private static ScholarshipCalculatorImpl scholarshipCalculator;
+
+    @BeforeClass
+    public static void initScholarshipCalculator() {
+        scholarshipCalculator = new ScholarshipCalculatorImpl();
+    }
+
+    @Test(timeout = 50)
+    public void scholarshipCalculateTest() {
+        for (int i = 1; i < 100_000; i++) {
+            double stepUpCoefficient = 1 / i;
+            double expected = 100 * stepUpCoefficient;
+            double actual = scholarshipCalculator.scholarshipCalculate(stepUpCoefficient);
+            Assert.assertEquals(expected, actual, 0.01);
+        }
+    }
+}
