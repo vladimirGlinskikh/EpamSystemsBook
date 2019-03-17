65cd3dfbd7b56e01bb047df8e41b4334190c26a1
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-17 13:28:33 +0600

    Метод SterUpCoefficient(int averageMark) возвращает повышающий коэффициент стипендии в зависимости от среднего бала студента.

--- src/main/java/application_1/calculation/ScholarshipCalculatorImpl.java
+++ src/main/java/application_1/calculation/ScholarshipCalculatorImpl.java
@@ -7,4 +7,22 @@ public class ScholarshipCalculatorImpl implements IscholarshipCalculator {
     public double scholarshipCalculate(double stepUpCoefficient) {
         return BASIC_SCHOLARSHIP * stepUpCoefficient;
     }
+
+    public double stepUpCoefficientCalculate(int averageMark) {
+        double stepUpCoefficient;
+        switch (averageMark) {
+            case 3:
+                stepUpCoefficient = 1;
+                break;
+            case 4:
+                stepUpCoefficient = 1.3;
+                break;
+            case 5:
+                stepUpCoefficient = 1.5;
+                break;
+            default:
+                stepUpCoefficient = 0;
+        }
+        return stepUpCoefficient;
+    }
 }
